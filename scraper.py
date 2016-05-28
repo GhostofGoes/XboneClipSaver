f5eb11909a6aaa133d5e9a8495841531fcb23f21
Christopher Goes <ghostofgoes@gmail.com>
2016-05-28 12:20:00 -0600

    Class and logging
    rewrote as class, added logging functionality, better handling of errors

--- scraper.py
+++ scraper.py
@@ -1,81 +1,94 @@
+#!/usr/bin/env python3
 # scraper.py
 
 import xbox
 import os
+import logging
 import urllib
 import argparse
 
-# Downloads all game clips to a given folder
-def save_clips(save_folder, clips):
-    if not os.path.exists(save_folder):
-        os.mkdir(save_folder)
-        print("Created folder.")
-    else:
-        print("Folder already exists, continuing...")
-    clip_count = 0  # I can't python
-    for clip in clips:
-        clip_json = clip.raw_json
-        download_url = clip_json["gameClipUris"][0]["uri"]
-        # video_name = clip_json["titleName"] + clip_json["dateRecorded"]
-        video_name = "video" + clip_count.__str__()  # Like I said, I can't python
-        file_name = os.path.join(save_folder, video_name + ".mp4")
-        if not os.path.isfile(file_name):
-            urllib.request.urlretrieve(download_url, file_name)
+
+class XboxScraper:
+
+    def __init__(self, silent=False, clip_folder="clips", screenshot_folder="screenshots"):
+        if silent:
+            logging.basicConfig(filename='XboxScraper.log', level=logging.INFO)
         else:
-            print("Error, file \"" + file_name + "\" already exists!")
-        clip_count += 1
+            logging.basicConfig(level=logging.INFO)
+        self.clip_folder = clip_folder
+        self.make_folder(clip_folder)
+        self.screenshot_folder = screenshot_folder
+        self.make_folder(screenshot_folder)
 
+    def save_clips(self, clips):
+        """ Downloads all game clips to a given folder """
+        clip_count = 0  # I can't python
+        for clip in clips:
+            clip_json = clip.raw_json
+            download_url = clip_json["gameClipUris"][0]["uri"]
+            # video_name = clip_json["titleName"] + clip_json["dateRecorded"]
+            video_name = "video" + clip_count.__str__()  # Like I said, I can't python
+            file_name = os.path.join(self.save_folder, video_name + ".mp4")
+            if not os.path.isfile(file_name):
+                urllib.request.urlretrieve(download_url, file_name)
+            else:
+                logging.error("Error, file %s already exists!", file_name)
+            clip_count += 1
 
-# Downloads all screenshots to a given folder
-def save_screenshots(save_folder, screenshots):
-    if not os.path.exists(save_folder):
-        os.mkdir(save_folder)
-        print("Created folder.")
-    else:
-        print("Folder already exists, continuing...")
-    screenie_count = 0
+    # TODO: implement screenshot saving, possibly using code in old/scraper2.py
+    def save_screenshots(self, screenshots):
+        """ Downloads all screenshots to a given folder """
+        screenie_count = 0
 
+    def user_prompts(self):
+        self.user = input("Enter username: ")
+        self.pwd = input("Enter password: ")
+        self.gtag = input("Enter gamertag: ")
 
-# Prompt user for things
-def user_prompts():
-    user = input("Enter username: ")
-    pwd = input("Enter password: ")
-    gtag = input("Enter gamertag: ")
+        self.login()
+        try:
+            logging.info("Attempting to pull gamertag info for the gamertag %s", self.gtag)
+            gt = xbox.GamerProfile.from_gamertag(self.gtag)
+        except xbox.exceptions.GamertagNotFound:
+            logging.error("Gamertag not found!")
 
-    try:
-        print("Attempting to authenticate...")
-        xbox.client.authenticate(login=user, password=pwd)
-    except:
-        print("Authentication failed!")
-    else:
-        print("Authentication successful!")
+        clips = list(gt.clips())
+        screenshots = []
+        self.save_clips(clips)
+        self.save_screenshots(screenshots)
 
+    def login(self):
+        try:
+            logging.info("Attempting to authenticate...")
+            xbox.client.authenticate(login=self.user, password=self.pwd)
+        except xbox.exceptions.AuthenticationException:
+            logging.error("Authentication failed!")
+        else:
+            logging.info("Authentication successful!")
 
-    gt = xbox.GamerProfile.from_gamertag(gtag)
+    @staticmethod
+    def make_folder(save_folder):
+        if not os.path.exists(save_folder):
+            os.mkdir(save_folder)
+            logging.info("Created folder %s", save_folder)
+        else:
+            logging.info("Folder %s already exists, continuing...", save_folder)
 
-    save_folder = input("Enter name of folder you want to save user's clips to: ")
-
-    clips = gt.clips()
-    clips = list(clips)
-    save_clips(save_folder, clips)
-
-# Don't prompt user for things, assume set in command line args or a config file
-def unattend():
 
 # TODO: theaded saving of clips, etc
-# TODO: commandline arguments
+# TODO: settings file or command line arg to set class variables instead of prompts
 def main():
-    prompt = True
+    no_prompt = False
+    clip_save_folder = "clips"
+    screenshot_save_folder = "screenshots"
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("--no-prompt", action='store_false', dest=prompt, help="Run the program without user prompts")
+    parser.add_argument("--no-prompt", action='store_true', dest=no_prompt, help="Run the program without user prompts")
+    # TODO: argument for settings file
 
-    if(prompt):
-        user_prompts()
-    else:
-        unattend()
+    scraper = XboxScraper()
+    scraper.user_prompts()
 
-
-# I like to control my pythons, ok?
 if __name__ == "__main__":
     main()
 
